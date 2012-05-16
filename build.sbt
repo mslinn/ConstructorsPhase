555ddd90f57201342a9ea7a639e52f9adba9dff8
Mike Slinn <mslinn@micronauticsresearch.com>
2012-05-16 13:11:02 -0700

    Updated to Scala 2.9.2

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "ConstructorsPhase"
 
 version := "0.1"
 
-scalaVersion := "2.9.1"
+scalaVersion := "2.9.2"
 
 scalacOptions ++= Seq("-deprecation")
 
@@ -13,12 +13,12 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "1.6.1" % "test" withSources()
+  "org.scalatest" %% "scalatest" % "1.7.2" % "test" withSources()
 )
 
 // Only show warnings and errors on the screen for compilations.
 // This applies to both test:compile and compile and is Info by default
-logLevel := Level.Error
+//logLevel := Level.Error
 
 logLevel in compile := Level.Warn
 
---
+++ src/main/scala/MyClass2.scala.scala
@@ -0,0 +1,47 @@
+/**
+  * Example program to sort out trait/case class construction.
+  * MyTrait's x, y and z properties are erased by the overrides in MyCaseClass and the
+  * anonymous class that it extends.
+  * Scala does not allow vars to be used for any of the properties of this example.
+  *
+  * Output is:
+  * {{{
+  * myCaseClass = MyCaseClass(X set by case class construction,Y default)
+  * myCaseClass.x=X set by case class construction
+  * myCaseClass.y=Y default
+  * myCaseClass.z=Z set by the initialization of the anonymous class extended by MyCaseClass
+  * MyTrait.x=X set by case class construction
+  * myCaseClass2 = MyCaseClass(X set by case class construction,Y set by case class construction)
+  * }}}
+  * @see http://lampwww.epfl.ch/~magarcia/ScalaCompilerCornerReloaded/2011Q2/ConstrPhase.pdf
+  * @author Mike Slinn
+  */
+
+trait MyTrait {
+  /** Cannot override a mutable variable so this cannot be a var */
+  val x: String = "X set by MyTrait initializaton"
+  val y: String = "Y set by MyTrait initializaton"
+  val z: String = "Z set by MyTrait initializaton"
+
+  def showX = println("MyTrait.x=" + x)
+}
+
+/** @param x erases MyTrait.x and completely replaces it, even from the point of view of methods
+ *           defined in the trait
+ *  @param y erases MyTrait.y and completely replaces it, even from the point of view of methods
+ *           defined in the trait */
+case class MyCaseClass(override val x: String, override val y: String = "Y default") extends {
+  override val z: String = "Z set by the initialization of the anonymous class extended by MyCaseClass"
+} with MyTrait
+
+object Main extends App {
+  val myCaseClass = MyCaseClass("X set by case class construction")
+  println("myCaseClass = " + myCaseClass)  // z (defined in the trait but not overridden) is not printed
+  println("myCaseClass.x=" + myCaseClass.x)
+  println("myCaseClass.y=" + myCaseClass.y)
+  println("myCaseClass.z=" + myCaseClass.z)
+  myCaseClass.showX
+
+  val myCaseClass2 = MyCaseClass("X set by case class construction", "Y set by case class construction")
+  println("myCaseClass2 = " + myCaseClass2) // z (defined in the trait but not overridden) is not printed
+}
